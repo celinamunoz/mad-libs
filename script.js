2c6504dcac99d86830b8224d9bd69425a33e9c06
celinamunoznyc <munoz.celina@gmail.com>
2017-01-10 22:55:24 +0000

    my commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// JavaScript File
+
+var answers= {
+    food:"pizza",
+    animals:"lions", 
+    things: "socks",
+    professional: "teacher",
+    clothingP: "shirt",
+    things2:"bobby pins",
+    person: " Celina",
+    place: " Rome",
+    verbing: "running",
+    feeling: "sad"
+    
+}
+var story= "Say " + answers.food+ 
+" the photographer said as the camera flashed!"+ 
+answers.person + "and I had gone to" + answers.place+
+"to get our photos taken today. The first photo we really wanted was a picture of us dressed as" + answers.animals+
+"pretending to be a" +answers.professional+
+"When we saw the proofs of it, I was a bit"+ answers.feeling +
+"because it looked different than I had imagined it would. I hadn’t imagined so many" + answers.things+
+"behind us. However, the second photo was exactly what I wanted. We both looked like" +
+answers.things2+ "wearing" + answers.clothingP + "and " + answers.verbing +
+"which was exactly what I had in mind!"
+
+console.log(story);
